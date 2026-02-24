eebce0bc6a729d1c5a47d66281eff802eb0d7e16
bobbybwoy <68812513+bobbybwoy@users.noreply.github.com>
2026-02-24 13:39:21 +0000

    Implemented the Home page
    - Created a new file, hamburger.js, which contains the code for the hamburger menu, which was previously in index.js
    
    - Updated index.js to refactor the code into hamburger.js and introduce the sessionStorage object, `userProfile`
    
    - Updated the SCSS files to ensure the hamburger menu can display on screens other than mobile devices

---
+++ hamburger.js
@@ -0,0 +1,48 @@
+const LAPTOP_MONITOR_WINDOW_SIZE = 1081;
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu__item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".hamburger__close-icon");
+const menuIcon = document.querySelector(".hamburger__menu-icon");
+
+const setMenuItemEventListener = (menuItem) => {
+    menuItem.removeEventListener("click", toggleMenu);
+    if (window.innerWidth < LAPTOP_MONITOR_WINDOW_SIZE)
+        menuItem.addEventListener("click", toggleMenu);
+};
+
+const toggleMenu = () => {
+    if (menu.classList.contains("menu--show")) {
+        menu.classList.remove("menu--show");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("menu--show");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+menuItems.forEach(menuItem => {
+    if (userProfile.isLoggedIn) {
+        if (menuItem.classList.contains("menu__item--loggedIn"))
+            menuItem.style.display = "block";
+        if (menuItem.classList.contains("menu__item--loggedOut"))
+            menuItem.style.display = "none";
+    } else {
+        if (menuItem.classList.contains("menu__item--loggedIn"))
+            menuItem.style.display = "none";
+        if (menuItem.classList.contains("menu__item--loggedOut"))
+            menuItem.style.display = "block";
+    }
+
+    setMenuItemEventListener(menuItem);
+});
+
+window.onresize = () => {
+    menuItems.forEach(menuItem => {
+        setMenuItemEventListener(menuItem);
+    });
+}
+
+hamburger.addEventListener("click", toggleMenu);
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const userProfileStr = sessionStorage.getItem("userProfile") ?? `{username:"",isLoggedIn:}`;
-const userProfile = JSON.parse(userProfileStr);
-
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menu__item");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".hamburger__close-icon");
-const menuIcon = document.querySelector(".hamburger__menu-icon");
-
-const toggleMenu = () => {
-    if (menu.classList.contains("menu--show")) {
-        menu.classList.remove("menu--show");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("menu--show");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
+// Attempt to get the user profile from sessionStorage.
+// If it does not exist, we will create one
+let userProfileStr = sessionStorage.getItem("userProfile");
+if (userProfileStr === null) {
+    console.log("We ain't got jack!");
+    userProfileStr = `{"name": "", "isLoggedIn": false}`;
+    sessionStorage.setItem("userProfile", userProfileStr);
 }
-
-menuItems.forEach(menuItem => menuItem.addEventListener("click", toggleMenu))
-
-hamburger.addEventListener("click", toggleMenu);
+const userProfile = JSON.parse(userProfileStr);
