8a5ec448cc4a7233c3b3fe316c92ce399aeee4e4
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-02-24 15:45:55 +0000

    user things

---
+++ data/fakeUser.js
@@ -0,0 +1,45 @@
+const fakeUserSessionData = {
+  name: "Remi's account",
+  isLoggedIn: true,
+};
+
+export const fakePosts = [
+  {
+    imageUrl: "https://placedog.net/600/400",
+    description: "That's Chenghong's dog. It's cute, isn't it?",
+    location: "Victoria Park, London",
+    breed: "Malamute",
+    likes: 101,
+    dislikes: 3,
+    createdAt: "2026-02-20T10:30:00",
+  },
+  {
+    imageUrl: "https://placedog.net/600/401",
+    description: "Kayley is my boss till we get out to the park. he he.",
+    location: "Shoreditch, London",
+    breed: "Schnauzer",
+    likes: 100,
+    dislikes: 1,
+    createdAt: "2026-02-21T14:15:00",
+  },
+  {
+    imageUrl: "https://placedog.net/600/402",
+    description: "Bod gave me the best treats. All I had to do was to pose for a photo.",
+    location: "Hyde Park, London",
+    breed: "Golden Retriever",
+    likes: 141,
+    dislikes: 1,
+    createdAt: "2026-02-22T09:00:00",
+  },
+  {
+    imageUrl: "https://placedog.net/600/403",
+    description: "I keep on asking Niko to get me out of the flat. I need to meet other dogs and sniff around.",
+    location: "Battersea Park, London",
+    breed: "Corgi",
+    likes: 50,
+    dislikes: 5,
+    createdAt: "2026-02-24T16:45:00",
+  },
+];
+
+export default fakeUserSessionData;
