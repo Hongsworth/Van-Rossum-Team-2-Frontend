a36c0c0274893b2600f61babbccef3fedd46f708
Hongsworth <therchgaming@gmail.com>
2026-02-25 13:53:56 +0000

    add slight styling to login page, fixed url in menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,13 @@
 export const getMenu = () => {
     return `<ul class="menu">
-            <li><a href="/index.html" class="menu__item">Home</a></li>
-            <li><a href="/pages/grid/grid.html" class="menu__item">Feed</a></li>
+            <li><a href="Van-Rossum-Team-2-Frontend/index.html" class="menu__item">Home</a></li>
+            <li><a href="Van-Rossum-Team-2-Frontend/pages/grid/grid.html" class="menu__item">Feed</a></li>
             <li><a href="#"
                     class="menu__item menu__item--loggedOut">Login/Register</a>
             </li>
             <li><a href="#" class="menu__item menu__item--loggedIn">Add a
                     Post</a></li>
-            <li><a href="/pages/user-profile/user-profile.html" class="menu__item">Profile</a>
+            <li><a href="Van-Rossum-Team-2-Frontend/pages/user-profile/user-profile.html" class="menu__item">Profile</a>
             </li>
             <li><a href="#" class="menu__item menu__item--loggedIn">Logout</a>
             </li>
